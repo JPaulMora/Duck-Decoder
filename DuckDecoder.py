276df30176b2c626f832be1a70d33ef55ad61707
John M <johmora12@engineer.com>
2015-04-19 20:31:40 -0600

    Added license notice and email.

--- DuckDecoder.py
+++ DuckDecoder.py
@@ -1,12 +1,27 @@
 ############## DuckDecoder ##############
 #  
 #  Description:   Python script to decode/ display usb rubber ducky inject.bin files
-#    Author(s):   JPaulMora (@jpaulmora)
-#      Version:   0.1.b
+#    Author(s):   JPaulMora (@jpaulmora) 
+#      Version:   0.1.b                
+#                                              Copyright (C) 2015  Juan Pablo Mora
+#    
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
 #
-#   Program currently supports all characters on an english keyboard mapping, I have been getting trouble identifying key
-#   combinations such as ALT ENTER U since its output in hex is the same as of a STRING u command. This also happens with
-#   UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW. they get translated to (STRING) u,d,l,r respectively.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#  Program currently supports all characters on an english keyboard mapping, I have been getting trouble identifying key
+#  combinations such as ALT ENTER U since its output in hex is the same as of a STRING u command. This also happens with
+#  UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW. they get translated to (STRING) u,d,l,r respectively.
 
 
 import binascii 
